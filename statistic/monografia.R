f71d3de48c36d98c79d8584a5baa1f2cab157d51
kureshio <clesioteixeira@outlook.com>
2023-07-04 10:20:15 -0300

    adicionar arquivos

--- statistic/monografia.R
+++ statistic/monografia.R
@@ -1,12 +1,12 @@
 
 #Importando os dados no Linux
-dados <- read.csv("/home/kureshio/Planilhas/texto/naine-diametro.csv",header=TRUE,dec=".",sep=";")
+dados <- read.csv("/home/kureshio/Planilhas/texto/maca-altura.csv",header=TRUE,dec=".",sep=";")
 
 # Importando o arquivo de dados no Windows.
-dados<- read.csv("P:\\Planilhas\\texto\\gorutuba-diametro.csv",header=TRUE, sep=";", dec=".")
+dados<- read.csv("P:\\Planilhas\\texto\\naine-altura.csv",header=TRUE, sep=";", dec=".")
 
 # Obtendo as médias da váriavel dados
-medias_por_categoria <- aggregate(dados[, -1], by = list(categoria = dados[, 1]), FUN = mean, na.rm=TRUE, na.action=NULL)
+medias_por_categoria <- aggregate(dados[, -1], by = list(Trat = dados[, 1]), FUN = mean, na.rm=TRUE, na.action=NULL)
 
 #Há dados com informação "NA". Para realizar a estatística, não pode ter esse tipo de informação
 medias_por_categoria[7,6] <- rowMeans(medias_por_categoria[7,2:7], na.rm=T)
@@ -15,13 +15,13 @@ medias_por_categoria[7,7] <- rowMeans(medias_por_categoria[7,2:7], na.rm=T)
 # Reformatar o objeto medias_por_categoria para deixar os dados em condições de leitura para as análises. 
 # Para isso, utilizaremos o comando pivot_longer que pertence à biblioteca tidyr.
 library(tidyr)
-tabela_transformada <- pivot_longer(medias_por_categoria, cols = c("R1", "R2", "R3", "R4", "R5", "R6","R7","R8"), names_to = "Dimensao", values_to = "Media")
+tabela_transformada <- pivot_longer(medias_por_categoria, cols = c("R1", "R2", "R3", "R4", "R5", "R6","R7"), names_to = "Rep", values_to = "Media")
 
 # retirando as linhas que tem valores NA
 tabela_transformada <- na.omit(tabela_transformada)
 
 # Agregando os valores em tratamento e repetição com suas médias
-tabela_media <- aggregate(tabela_transformada$Media, by = list(tabela_transformada$TRAT,tabela_transformada$Dimensao), FUN=mean)
+tabela_media <- aggregate(tabela_transformada$Media, by = list(tabela_transformada$Trat,tabela_transformada$Rep), FUN=mean)
 tabela_ordenada <- tabela_media[order(tabela_media$Group.1),]
 
 # Alterando os nomes das colunas para facilitar a vida nos próximos comandos.
@@ -37,7 +37,16 @@ colnames(tabela_transformada)[3] <- "Media"
 
 # Utilizando o pacote ExpDes.pt para realizar a análise de variância.
 library(ExpDes.pt)
-dic(tabela_transformada$Trat,tabela_transformada$Media,mcomp = "sk", quali=FALSE)
+dic(tabela_transformada$Trat ,tabela_transformada$Media,mcomp = "sk", quali=FALSE)
+
+modelo <- aov(Media ~ Trat, data = tabela_transformada)
+
+summary(modelo)
+
+tabela_transformada$Trat <- factor(tabela_transformada$Trat)
+
+sapply(tabela_transformada, summary)
+
 
 # Teste
 plot(data = tabela_transformada, Media ~ Trat)
